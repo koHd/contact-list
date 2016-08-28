6dcda7f02b2727251ef5008068f444123b9e7e3d
Shane Farrell <shane.farrell@yahoo.ie>
2016-08-28 17:42:00 +0100

    ejs hello world

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var testController = require('./controllers/test.js');
+
 
 var app = express();
 
+// middleware
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// routing
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('test');
 });
 
 app.listen(8001, function() {
