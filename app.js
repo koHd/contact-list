989ec12617012166f52a9240d3629ecef07763d1
Shane Farrell <shane.farrell@yahoo.ie>
2016-08-29 16:29:36 +0100

    removed test code

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // routing
 app.get('/', function (req, res) {
-  res.send("Hello World!");
+  res.redirect('/contacts');
 });
 
 app.use('/contacts', contactsController);
